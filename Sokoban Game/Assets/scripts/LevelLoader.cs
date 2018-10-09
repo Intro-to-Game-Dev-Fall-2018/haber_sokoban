fd52d05b0a8de136fb15edf5074818297f151dcc
Benjamin Haber <benhasanemail@gmail.com>
2018-10-09 16:47:00 -0400

    level loader works, added buttons

--- Sokoban Game/Assets/scripts/LevelLoader.cs
+++ Sokoban Game/Assets/scripts/LevelLoader.cs
@@ -1,50 +1,71 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
-    [Header("Inputs")] [SerializeField] private TextAsset _levelSet;
+    [Header("Inputs")] 
+    [SerializeField] private TextAsset _levelSet;
 
-    [Header("Assets")] [SerializeField] private GameObject _wall;
-    [SerializeField] private GameObject _box;
+    [Header("Assets")] 
+    [SerializeField] private GameObject _wall;
+    [SerializeField] private GameObject _package;
     [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _goal;
     [SerializeField] private GameObject _floor;
 
-    private GameObject[][] _map;
     private string[] _levels;
     private int currentLevel;
 
-    private void nextLevel()
+    public void nextLevel()
     {
-        var lines = _levels[currentLevel].Split('\n');
-        var size = lines.Select(line => line.Length).Concat(new[] {0}).Max();
-        _map = new GameObject[lines.Length][];
+        if (currentLevel >= _levels.Length) return;
 
-        for (var i = 0; i < _map.Length; i++)
+        foreach (Transform child in transform) {
+            Destroy(child.gameObject);
+        }
+        
+        loadLevel();
+    }
+    
+    private void loadLevel()
+    {
+        var lines = _levels[currentLevel++].Split('\n');
+        
+        for (var i = 0; i < lines.Length; i++)
         {
-            _map[i] = new GameObject[size];
+            if (lines[i].StartsWith(";")) continue;
             for (var j = 0; j < lines[i].Length; j++)
             {
+                var pos = new Vector3(j,-i,0f);
+                
+                // ReSharper disable once SwitchStatementMissingSomeCases
                 switch (lines[i][j])
                 {
                     case '#':
-                        _map[i][j] = Instantiate(_wall);
+                        Instantiate(_wall,pos,Quaternion.identity).transform.SetParent(transform);
                         break;
                     case '@':
-                        _map[i][j] = Instantiate(_player);
+                        Instantiate(_player,pos,Quaternion.identity).transform.SetParent(transform);
                         break;
                     case '$':
-                        _map[i][j] = Instantiate(_box);
+                        Instantiate(_package,pos,Quaternion.identity).transform.SetParent(transform);
                         break;
-                    default:
-                        _map[i][j] = Instantiate(_floor);
+                    case '.':
+                        Instantiate(_goal,pos,Quaternion.identity).transform.SetParent(transform);
+                        break;
+                    case '+': 
+                        Instantiate(_player,pos,Quaternion.identity).transform.SetParent(transform);
+                        Instantiate(_goal,pos,Quaternion.identity).transform.SetParent(transform);
+                        break;
+                    case '*':
+                        Instantiate(_package,pos,Quaternion.identity).transform.SetParent(transform);
+                        Instantiate(_goal,pos,Quaternion.identity).transform.SetParent(transform);
                         break;
                 }
+                Instantiate(_floor,pos,Quaternion.identity).transform.SetParent(transform);
             }
         }
+
     }
 
     private void Start()
@@ -52,10 +73,7 @@ public class LevelLoader : MonoBehaviour
         string[] split = {"\n\n"};
         currentLevel = 0;
         _levels = _levelSet.text.Split(split, StringSplitOptions.RemoveEmptyEntries);
+        nextLevel();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    
 }
---
+++ Sokoban Game/Assets/scripts/playerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : ScriptableObject
+{
+	public int moves;
+
+
+	public void reset()
+	{
+		moves = 0;
+	}
+}
