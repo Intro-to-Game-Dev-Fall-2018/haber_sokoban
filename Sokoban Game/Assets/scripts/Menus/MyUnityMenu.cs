13bf46b6ed7484a1729fffe8350cdf05fd95e679
Benjamin Haber <benhasanemail@gmail.com>
2018-10-22 17:47:47 -0400

    added custom tools to unity editor

---
+++ Sokoban Game/Assets/scripts/Menus/MyUnityMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MyUnityMenu : ScriptableObject {
+
+    [MenuItem("Tools/Clear PlayerPrefs")]
+    private static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+    
+}
